f4af7ad984494fcf8fefdc5d53a3eeb991fc21f1
Joevohan261 <84632709+okaisank@users.noreply.github.com>
2024-11-22 12:59:43 +0700

    Update MyClass.h

--- MyClass.h
+++ MyClass.h
@@ -1,15 +1,21 @@
 #ifndef MyClass_h
 #define MyClass_h
 
-
 #include "Arduino.h"
 
 class MyClass {
   public:
      MyClass(int pin);
-     void myFunction(int blinkRate); // กำหนด วินาที
+    // ฟังก์ชันกระพริบ LED
+    void myFunction(int blinkRate); // กำหนดอัตราการกระพริบ (ms)
+
+    // ฟังก์ชันสำหรับปุ่มกด
+    void setButtonPin(int buttonPin); // กำหนดขา Pin สำหรับปุ่ม
+    void myButtonFunction();         // ควบคุม LED ตามสถานะปุ่ม
   private:
      int _pin;
+	 int _buttonPin ;
+	 int _buttonState;   // สถานะของปุ่ม (HIGH หรือ LOW)
 };
 
 #endif
