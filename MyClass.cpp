37b13f9eb9cc2323f504dfd62431613fa2f4d8fd
Joevohan261 <84632709+okaisank@users.noreply.github.com>
2024-11-23 13:15:16 +0700

    Update MyClass.cpp

--- MyClass.cpp
+++ MyClass.cpp
@@ -5,14 +5,8 @@
 MyClass::MyClass(int pin) {
   pinMode(pin, OUTPUT);
   _pin = pin;
-  _buttonState = LOW; // กำหนดค่าเริ่มต้น Pull-Down use Resistor 4RK = 4.7K Or 10K
 }
 
-// ฟังก์ชันตั้งค่า pushbutton
-void MyClass::setButtonPin(int buttonPin) {
-  pinMode(buttonPin, INPUT);// Thise circuit use Pull Down for status 0 , ตรงข้ามกันถ้าใช้ INPUT_PULLUP status 1.
-  _buttonPin = buttonPin; // เก็บค่าขา Pin ของปุ่ม
-}
 
 // ฟังก์ชันกระพริบ LED
 void MyClass::myFunction(int blinkRate) {
@@ -22,14 +16,4 @@ void MyClass::myFunction(int blinkRate) {
   delay(blinkRate);
 }
 
-// ฟังก์ชันทำงานเมื่อปุ่มถูกกด
-void MyClass::myButtonFunction() {
-  _buttonState = digitalRead(_buttonPin); // อ่านสถานะปุ่ม
-  if (_buttonState == HIGH) {
-    // เปิด LED
-    digitalWrite(_pin, HIGH);
-  } else {
-    // ปิด LED
-    digitalWrite(_pin, LOW);
-  }
-}
+
