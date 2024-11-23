74b4a9d9c70db3b460274d33017a992cc83fcbc8
Joevohan261 <84632709+okaisank@users.noreply.github.com>
2024-11-23 13:16:17 +0700

    Update MyClass.h

--- MyClass.h
+++ MyClass.h
@@ -9,13 +9,9 @@ class MyClass {
     // ฟังก์ชันกระพริบ LED
     void myFunction(int blinkRate); // กำหนดอัตราการกระพริบ (ms)
 
-    // ฟังก์ชันสำหรับปุ่มกด
-    void setButtonPin(int buttonPin); // กำหนดขา Pin สำหรับปุ่ม
-    void myButtonFunction();         // ควบคุม LED ตามสถานะปุ่ม
   private:
      int _pin;
 	 int _buttonPin ;
-	 int _buttonState;   // สถานะของปุ่ม (HIGH หรือ LOW)
 };
 
 #endif
